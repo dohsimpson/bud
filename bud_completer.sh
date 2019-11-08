9cad2658f55339198ebe9b170f52a84ef28d5ac2
dohsimpson <mydohsimpson@gmail.com>
2019-11-08 11:41:10 -0500

    Add budz for zsh support

--- bud_completer.sh
+++ bud_completer.sh
@@ -2,4 +2,10 @@ _bud() {
   local cur=${COMP_WORDS[COMP_CWORD]}
   COMPREPLY=($(bash -c "source Budfile >/dev/null 2>&1 && compgen -A function $cur"))
 }
+
+_budz() {
+  local cur=${COMP_WORDS[COMP_CWORD]}
+  COMPREPLY=($(zsh -c 'source Budfile >/dev/null 2>&1 && echo ${(k)functions[@]}'))
+}
 complete -F _bud bud
+complete -F _budz budz
