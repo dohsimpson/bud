adcee02a654fd7aeb5d1f9af55520bd760c97b81
dohsimpson <mydohsimpson@gmail.com>
2019-11-09 14:39:21 -0500

    fix

--- bud_completer.sh
+++ bud_completer.sh
@@ -5,7 +5,7 @@ _bud() {
 
 _budz() {
   local cur=${COMP_WORDS[COMP_CWORD]}
-  COMPREPLY=($(zsh -c 'source Budfile >/dev/null 2>&1 && echo ${(k)functions[@]}'))
+  COMPREPLY=($(zsh -c 'source Budzfile >/dev/null 2>&1 && echo ${(k)functions[@]}'))
 }
 complete -F _bud bud
 complete -F _budz budz
