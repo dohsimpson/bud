52ad9ada0108b1c680f2c9cc5e31230f1705a495
dohsimpson <mydohsimpson@gmail.com>
2020-06-13 10:24:23 -0400

    fix bud_completer.sh

--- bud_completer.sh
+++ bud_completer.sh
@@ -1,11 +1,13 @@
 _bud() {
   local cur=${COMP_WORDS[COMP_CWORD]}
-  COMPREPLY=($(bash -c "source Budfile >/dev/null 2>&1 && compgen -A function $cur"))
+  local BUDFILE="${BUDFILE:-Budfile}"
+  COMPREPLY=($(bash -c "source $BUDFILE >/dev/null 2>&1 && compgen -A function $cur"))
 }
 
 _budz() {
   local cur=${COMP_WORDS[COMP_CWORD]}
-  COMPREPLY=($(zsh -c 'source Budzfile >/dev/null 2>&1 && echo ${(k)functions[@]}'))
+  local BUDFILE="${BUDFILE:-Budzfile}"
+  COMPREPLY=($(zsh -c "source $BUDFILE >/dev/null 2>&1 && echo ${(k)functions[@]}"))
 }
 complete -F _bud bud
 complete -F _budz budz
